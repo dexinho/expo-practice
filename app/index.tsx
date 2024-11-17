56a0bf7454bb1fcf4cc682ef0a7aca84379ddff1
Jasmin Hidanovic <dexinho008@gmail.com>
2024-11-18 00:05:40 +0100

    reseting project

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Welcome = () => {
+  return <View>Welcome page</View>;
+};
+
+export default Welcome;
